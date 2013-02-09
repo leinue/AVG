c5f048072eb0f84671217a5da6281a3c336bf1a3
leinue <597055914@qq.com>
2013-02-09 16:55:56 +0800

    edited

--- AVGEngine/OAEElement.vb
+++ AVGEngine/OAEElement.vb
@@ -1,20 +1,14 @@
-﻿<<<<<<< HEAD
 ﻿Public Structure OAEItem
-=======
 ﻿Structure OAEItem
->>>>>>> f8ce43f24ce004cc09422e36e3f12fb334add52c
     Dim type As String
     Dim locX As Integer
     Dim locY As Integer
     Dim height As Integer
     Dim width As Integer
     Dim action As String
-<<<<<<< HEAD
 End Structure
 Public Structure OAEWindow
     Dim bgImage As String
     Dim bgbgMusic As String
     Dim itemList As String
 End Structure
-End Structure
->>>>>>> f8ce43f24ce004cc09422e36e3f12fb334add52c
