38a3a8ab5dd60e691ca01b294ad22b706a04a6f7
TerryGeng <gengyanda@gmail.com>
2013-02-11 16:18:35 +0800

    Update 'OAEItem' to define max range of text

--- AVGEngine/OAEElement.vb
+++ AVGEngine/OAEElement.vb
@@ -2,25 +2,37 @@
     'General Info
     Dim name As String
     Dim type As String
+
+
     'Location Info
     Dim locX As Integer
     Dim locY As Integer
     Dim height As Integer
     Dim width As Integer
+
+
     'Action Info
     Dim ClickAction As String
     Dim HoverAction As String
+
+
     'Attributes of Image
     Dim NormalImage As String
     Dim HoverImage As String
     Dim ClickImage As String
+
+
     'Attributes of Text
     Dim NormalText As String
     Dim HoverText As String
     Dim ClickText As String
+
     Dim NormalFont As String
     Dim HoverFont As String
     Dim ClickFont As String
+
+    Dim TextMaxWidth As String
+    Dim TextMaxHeight As String
 End Structure
 Public Structure OAEWindow
     Dim name As String
